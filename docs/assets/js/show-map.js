d566f6399dccfd9df8d6af483fa8a871f9472e42
Paul Nijjar <pnijjar@alumni.uwaterloo.ca>
2019-02-26 00:56:41 -0500

    Simplify more, sigh.

--- docs/assets/js/show-map.js
+++ docs/assets/js/show-map.js
@@ -1,32 +1,6 @@
 
-var geojsonLayer = null;
-
-// This feature had better be from the geoJSON
-function getPopupText(feature) {
-  return feature.properties["Name"] + ": "
-    + feature.properties["information-link"];
-}
-
-function onEachFeature(feature, layer) {
-    if (feature.properties) {
-        if (feature.properties["information-link"]
-          && feature.properties["Name"]) {
-            layer.bindPopup(getPopupText(feature));
-        } // end if information link
-
-        layer.setStyle({
-            color: feature.properties["stroke"],
-            fillColor: feature.properties["fill"],
-            fillOpacity: feature.properties["fill-opacity"],
-            weight: feature.properties["stroke-width"],
-            opacity: feature.properties["opacity"]
-        });
-    }
-
-}
-
-
 var attrib = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+
 var map = new L.Map('map', {
     zoom: 10,
     center: new L.latLng([43.45850, -80.51511]),
@@ -51,36 +25,10 @@ var searchControl = new L.Control.Search({
     container: 'map-searchbar',
     zoom: 15,
     firstTipSubmit: true,
-    textPlaceholder: "Search by address v03",
+    textPlaceholder: "Search by address v04",
     minLength: 3
 });
 
 map.addLayer(baseLayer);
 map.addControl(searchControl);
-
-$.getJSON("./assets/data/WardBoundaries.geojson", function(data) {
-    var geojson = L.geoJson(data, {
-      onEachFeature: onEachFeature
-    });
-
-    geojsonLayer = geojson;
-
-
-    searchControl.on('search:locationfound', function(e) {
-        // Use Mapbox Leaflet PIP (point in polygon) library.
-        // Ugh ugh ugh.
-        // https://stackoverflow.com/questions/48798336/
-        var foundLayers = leafletPip.pointInLayer(e.latlng, geojsonLayer);
-
-        foundLayers.forEach(function(layer) {
-          layer.fire('click', {
-            latlng: e.latlng
-          });
-        });
-
-    });
-
-    // map.addLayer(geojson);
-
-});
 
