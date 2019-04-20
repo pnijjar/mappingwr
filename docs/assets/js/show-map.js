2284870e8506016befd4e3f1e30337ae5bd513bc
Paul Nijjar <pnijjar@alumni.uwaterloo.ca>
2019-04-20 00:57:35 -0400

    Try copy-and-pasting code off the Internet

--- docs/assets/js/show-map.js
+++ docs/assets/js/show-map.js
@@ -1,6 +1,22 @@
 
 var attrib = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
 
+
+function onEachFeature(feature, layer) {
+    if (feature.properties) {
+
+        layer.setStyle({
+            color: feature.properties["stroke"],
+            fillColor: feature.properties["fill"],
+            fillOpacity: feature.properties["fill-opacity"],
+            weight: feature.properties["stroke-width"],
+            opacity: feature.properties["opacity"]
+        });
+    }
+
+}
+
+
 var map = new L.Map('map', {
     zoom: 10,
     center: new L.latLng([43.45850, -80.51511]),
@@ -14,12 +30,31 @@ map.addLayer(baseLayer);
 
 var collisionLayer = null;
 
+var pointStyle = {
+  "color": "0000ff";
+  "weight": 1;
+}
+
+// https://leafletjs.com/examples/geojson/
+var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+};
+
+
+
+
 $.getJSON(
   "https://opendata.arcgis.com/datasets/daeb44b4880140f88561840efe3ccfbe_0.geojson",
   function(data) { 
-      console.log(data);
-        // collisionLayer = 
-        L.geoJson(data).addTo(map);
+        pointToLayer: function (feature, latlng) { 
+          return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+        map.addLayer(geojson);
       });
 
 
@@ -34,7 +69,7 @@ var searchControl = new L.Control.Search({
     container: 'map-searchbar',
     zoom: 15,
     firstTipSubmit: true,
-    textPlaceholder: "Search by address v14",
+    textPlaceholder: "Search by address v15",
     minLength: 3
 });
 
